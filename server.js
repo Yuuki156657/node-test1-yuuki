f502a9e6ca709f16060064f54255b91c3b4e981c
yukinagano <tawapeee16@gmail.com>
2019-09-15 22:49:14 -0700

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('./models/db');
 
-var PORT = process.env.PORT || 3000;
+// var PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const path = require('path');
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log('Express server started at port : 3000');
 });
 
